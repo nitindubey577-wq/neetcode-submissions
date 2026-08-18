1b532ef2971aafbebc08cec3912404288e9d1e36
nitindubey577-wq <nitindubey577@gmail.com>
2026-08-18 07:04:17 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+int help( int k , int[] nums ){
+   long ans =0;
+   for ( int i =0; i<nums.length; i++){
+      ans = ans + (nums[i] + k - 1L) / k;
+   }
+   return (int) Math.min(ans, Integer.MAX_VALUE);
+}
+    public int minEatingSpeed(int[] piles, int h) {
+        int max=0;
+        for(int p : piles) if(p > max) max = p;
+        int l =1;
+        int j=max;
+        int m=0;
+        int res = max;
+        while ( l <= j){
+         m = l + (j - l)/2;
+        if( help( m , piles) <= h ){
+         res = m;
+         j = m-1;
+        }
+      else {
+          l = m+1;
+      }
+        }
+        return res;
+          }
+}
